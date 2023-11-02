40bcdd6d94ad02dd6dd7902acde498fdf4848dfd
realisshomyang <realisshoman@kakao.com>
2023-11-02 22:35:55 +0900

    [FEATURE] everyware-server 도메인 entity 작성

---
+++ src/main/java/com/everyware/model/expo/entity/Expo.java
@@ -0,0 +1,35 @@
+package com.everyware.model.expo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "expo")
+public class Expo extends BaseEntity{
+
+    @Id
+    @Column(name = "expo_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "expo_image_url")
+    private String expoImageUrl;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "introduction")
+    private String introduction;
+
+    @OneToMany(mappedBy = "expo")
+    private List<Booth> booths = new ArrayList<>();
+}
